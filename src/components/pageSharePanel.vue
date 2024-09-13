86ded9a076c51a353b55fca7dd844fc0863cafa8
zhangxinlin <2427951641@qq.com>
2024-09-13 12:41:52 +0800

    备份

--- src/components/pageSharePanel.vue
+++ src/components/pageSharePanel.vue
@@ -28,16 +28,20 @@
                 </ul>
                 <a class="dp-bk br-1rem tc-66" @click="onClose">取消</a>
             </div>
-            <div v-if="qrDataURL" class="psp-qrcode-box" @click="onClose">
-                <img :src="qrDataURL" alt="扫码分享" class="psp-qrcode-pic" />
-                <span class="tc-66">请使用手机扫码</span>
-            </div>
+            <transition name="psp-pull-down">
+                <div v-if="qrDataURL" class="psp-qrcode-box" @click="onClose">
+                    <img :src="qrDataURL" alt="扫码分享" class="psp-qrcode-pic" />
+                    <span class="tc-66">请使用手机扫码</span>
+                    <canvas ref="canvasBox" class="psp-qrcode-canvas" :width="canvasSize" :height="canvasSize" />
+                    <!-- <span v-for="nth in dotCount" :key="nth" class="psp-rect-dot" :style="getDotStyle(nth)"></span> -->
+                </div>
+            </transition>
         </div>
     </transition>
 </template>
 
 <script setup name="PageSharePanel">
-    import { defineProps, defineModel, ref } from "vue";
+    import { defineProps, defineModel, ref, getCurrentInstance, nextTick } from "vue";
     import publicAssets from "@/assets/data/publicAssets.js";
     import qrcode from "qrcode";
     
@@ -46,9 +50,14 @@
         default: false
     });
     
+    const $instance = getCurrentInstance();
     const emits = defineEmits(["finished"]); //结束时调用函数
     const qrDataURL = ref(null);
     const isMobile = (navigator.userAgent.indexOf("Mobile") >= 0);
+    const dotCount = 900;
+    const canvasSize = window.pxOf1rem * 9; /* 等于二维码图片的宽高 */
+    const rectSize = 6; //像素。二维码中小块的宽高
+    const rectRows = Math.ceil(canvasSize / rectSize); //二维码有多少行
     
     function onClose(){
         isVisible.value = false;
@@ -102,6 +111,7 @@
             margin: 0,
         }).then(url => {
             qrDataURL.value = url;
+            nextTick(generateCanvasQrcode);
         }).catch(err => {
             appToast("生成二维码失败：" + err.message);
         });
@@ -141,9 +151,42 @@
         qrDataURL.value = null;
         emits("finished", 0xFF);
     }
+    
+    function generateCanvasQrcode(){
+        const ctx2d = $instance.refs.canvasBox.getContext("2d");
+        ctx2d.fillStyle = "#FFFFFF";
+        ctx2d.fillRect(0, 0, canvasSize, canvasSize);
+    }
+    
+    function getDotStyle(nth){
+        const idx = nth - 1;
+        const transX = 20 + Math.floor(idx % 30) * 6;
+        const transY = 20 + Math.floor(idx / 30) * 6;
+        const delay = 400 + nth * 5;
+        return `left:${transX}px;top:${transY}px;animation: psp-dot-fade-out 0ms ease ${delay}ms 1 forwards;`;
+        //return `transform:translate(${transX}px,${transY}px);animation: psp-dot-fade-out 0ms ease ${delay}ms 1 forwards;`;
+    }
 </script>
 
 <style>
+    @keyframes psp-qrcode-blur-kf{
+        from {
+            filter: blur(1rem);
+        }
+        to {
+            filter: blur(0);
+        }
+    }
+    
+    @keyframes psp-dot-fade-out{
+        from {
+            opacity: 1;
+        }
+        to {
+            opacity: 0;
+        }
+    }
+    
     .psp-box-container{
         position: fixed;
         left: 0;
@@ -177,7 +220,7 @@
     }
     .psp-qrcode-box{
         position: absolute;
-        top: 10vh;
+        top: 5vh;
         left: calc(50% - 5.5rem);
         z-index: 1;
         padding: 1rem 1rem 0.5rem  1rem;
@@ -190,6 +233,27 @@
         width: 9rem;
         height: 9rem;
         margin-bottom: 0.5rem;
+        /* filter: blur(1rem);
+        animation: psp-qrcode-blur-kf 400ms ease 200ms 1 forwards; */
+    }
+    .psp-qrcode-canvas{
+        display: block;
+        position: absolute;
+        left: 1rem;
+        top: 1rem;
+        z-index: 8;
+        width: 9rem;
+        height: 9rem;
+    }
+    .psp-rect-dot{
+        display: block;
+        position: absolute;
+        left: 1rem;
+        top: 1rem;
+        z-index: 8;
+        width: 6px;
+        height: 6px;
+        background-color: #fff;
     }
     
     .psp-slide-out-enter-from{opacity:0}
@@ -203,4 +267,11 @@
     .psp-slide-out-enter-to > .psp-box-dialog{transform:translateY(0%)}
     .psp-slide-out-leave-from > .psp-box-dialog{transform:translateY(0%)}
     .psp-slide-out-leave-to > .psp-box-dialog{transform:translateY(30%)}
+    
+    .psp-pull-down-enter-from{opacity:0;transform:translateY(-50%)}
+    .psp-pull-down-enter-active{transition:all 300ms cubic-bezier(0, 0, 0, 1) 0s}
+    .psp-pull-down-enter-to{opacity:1;transform:translateY(0%)}
+    .psp-pull-down-leave-from{opacity:1;transform:translateY(0%)}
+    .psp-pull-down-leave-active{transition:all 300ms cubic-bezier(0, 0, 0, 1) 0s}
+    .psp-pull-down-leave-to{opacity:0;transform:translateY(-50%)}
 </style>
