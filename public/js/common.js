26d4ff5d771e6d99b3d7b5aa4cf5cb8b9d8db9b4
zhangxinlin <2427951641@qq.com>
2024-12-26 00:17:28 +0800

    视频播放可以全屏显示

--- public/js/common.js
+++ public/js/common.js
@@ -1,5 +1,6 @@
 const IS_MOBILE = navigator.userAgent.indexOf("Mobile") >= 0; //是否是移动端
 const IS_H5MODE = (window.innerWidth < 768); //是否是h5模式
+const IS_WECHAT = navigator.userAgent.indexOf("MicroMessenger") >= 0; //是否是微信内置浏览器
 
 //适用于不处理 Promise catch 的情况，防止不处理 catch 而报错！
 function globalEmptyShell(){
--- src/views/index/map3dShareVideo.vue
+++ src/views/index/map3dShareVideo.vue
@@ -1,6 +1,21 @@
 <template>
     <div class="page-limit-width">
         <template v-if="shareInfos && shareInfos.id">
+            <video v-if="isWx"
+                src="@/assets/video/a_blood_moon.webm"
+                ref="tempVideoRef"
+                width="390"
+                height="480"
+                autoplay="true"
+                muted="true"
+                loop="true"
+                class="ps-f po-tl-0 zi-0 op-0">【2024年12月23日】
+            【注意】这个隐藏的视频控件的目的是：处理微信内置浏览器全屏时自动旋转的问题。
+            由于微信内置浏览器会根据视频的宽高（视频高度大于宽度时），全屏时会自动旋转视频。
+            别的浏览器调用 document.documentElement.requestFullScreen()，且视频高度大于宽度时，就不会自动旋转。
+            但是微信这个垃圾内置浏览器就自动旋转视频！我又不是掉用 video.requestFullScreen()，为啥给我旋转屏幕！
+            【这个控件必须放在页面顶部，就是页面内的第一个视频。我调用全屏的代码在组件 videoSwiperItem 里，不在这个页面】
+            【垃圾微信，坚决抵制，永不开发小程序，希望尽快倒闭，解放码牛】</video>
             <div class="content-cage" style="padding:0;overflow:hidden;background-color:#000;height:100%">
                 <video
                     :src="shareInfos.pictureList[0].picPath"
@@ -35,7 +50,7 @@
     import { ref, reactive, getCurrentInstance, onActivated, onDeactivated, computed, nextTick, onBeforeUnmount } from "vue";
     import { useStore } from "vuex";
     import { useRoute, useRouter } from "vue-router";
-    import { makeThemeColorDark, makeThemeColorLight } from "@/utils/pagehelper.js";
+    import { makeThemeColorDark, makeThemeColorLight, toggleFullScreen } from "@/utils/pagehelper.js";
     import videojs from "video.js";
     import myStorage from "@/utils/mystorage.js";
     import ajaxRequest from "@/request/index.js";
@@ -51,6 +66,8 @@
     const isLoading = ref(true);
     const isContentShrink = ref(false);
     const csbRef = ref(null);
+    const tempVideoRef = ref(null);
+    const isWx = (IS_MOBILE && IS_WECHAT);
     
     let videoHeight = 0;
     let videoPlayer = null;
@@ -70,11 +87,45 @@
         }
     }
     
+    //修复微信内置浏览器退出全屏后系统导航栏无法复原的问题！
+    function fixWechatBugAfterExitFullscreen(isRestore){
+        /** 【2024年12月25日 bug描述（本人使用三星手机S24+，其他厂商手机有没有这个问题暂不知道）】
+         * 微信内置浏览器全屏后，再次退出全屏时，底部系统导航栏无法还原，但是可以看到安卓的三个按键（系统导航栏变成了类似CSS的固定定位了）。导致遮住了视频底部进度条。
+         * 其他浏览器暂时没有发现这个问题。就是微信这个垃圾货有这个问题！
+         * 【2024年12月25日 解决方法】
+         * 退出全屏前，暂停播放所有视频。成功退出全屏后再重新播放所有视频
+         * 注意：比如当前页面有10部视频，那就必须在全屏前暂停播放这10部视频，退出全屏后再重新播放这些视频！
+        */
+        if(tempVideoRef.value && videoPlayer){
+            if(isRestore){
+                tempVideoRef.value.play();
+                videoPlayer.play();
+            } else {
+                tempVideoRef.value.pause();
+                videoPlayer.pause();
+            }
+        }
+    }
+    
     function onVideoClick(evt){
         //点击视频组件才能生效
         if(evt.target.tagName === "VIDEO"){
+            if(tempVideoRef.value && document.fullscreenElement){//移动端微信内置浏览器，并且是将要退出全屏时才处理
+                fixWechatBugAfterExitFullscreen(false);
+                setTimeout(fixWechatBugAfterExitFullscreen, 50, true);
+            }
             isContentShrink.value = !isContentShrink.value;
+            toggleFullScreen(isContentShrink.value);
             nextTick(() => videoPlayer.userActive(!csbRef.value.checkWhetherHide())); //随便隐藏或显示控制条
+        }
+    }
+    
+    function letsStartPlayVideo(){
+        if(tempVideoRef.value){//这块代码必须放在顶部
+            tempVideoRef.value.play();
+        }
+        if(videoPlayer && videoPlayer.paused()){
+            videoPlayer.play().catch(globalEmptyShell);
         }
     }
     
@@ -99,9 +150,10 @@
                     this.on("click", onVideoClick);
                 }
             });
+            setTimeout(letsStartPlayVideo, 100);
             console.log("创建了视频组件...");
         } else if(videoPlayer.currentSrc().endsWith(shareInfos.pictureList[0].picPath)){
-            videoPlayer.play();
+            letsStartPlayVideo();
             console.log("继续播放视频内容...");
         } else {
             //参考文档：https://docs.videojs.com/docs/api/player.html#Methodsposter
@@ -112,7 +164,7 @@
             });
             videoPlayer.poster(shareInfos.pictureList[0].thumbnailPath);
             videoPlayer.load();
-            videoPlayer.play();
+            letsStartPlayVideo();
             console.log("更新了视频内容...");
         }
     }
