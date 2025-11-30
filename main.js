b6e1f8dc3fb01cdcc8b5b55b6322262829054c24
z <54790998+real9gagger@users.noreply.github.com>
2025-11-30 15:04:06 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue';
+import { register as swiperRegister } from 'swiper/swiper-element-bundle';
+import App from './App.vue';
+import MyRouter from './router';
+import VuexStore from './store';
+import genzScrollView from '@/components/genzScrollView';
+import modernSearchBox from '@/components/modernSearchBox';
+
+swiperRegister();
+
+const myApp = createApp(App);
+
+myApp.component('GenzScrollView', genzScrollView); //Z世代风格的滚动视图（支持上拉加载和下拉刷新）
+myApp.component('ModernSearchBox', modernSearchBox); //搜索框
+
+myApp.use(MyRouter);
+myApp.use(VuexStore);
+myApp.mount('#xlttapp');
